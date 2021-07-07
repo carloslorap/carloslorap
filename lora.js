1a78f05d23413aeeb8de41a71349617174a2d339
carloslorap <74977007@certus.edu.pe>
2021-07-06 20:51:26 -0500

    comint

--- lora.js
+++ lora.js
@@ -6,3 +6,51 @@ moon.style.top = value * 0.70 + 'px';
 
 
 })
+
+window.addEventListener('load' , function () { 
+    console.log('el contenido ha cargado');
+    
+    var imagenes =[]
+    
+    imagenes[0]='diseño carlos/png1.png'
+    imagenes[1]='diseño carlos/png2.png'
+    
+    
+    var indiceImagenes=0;
+    
+    function cambiarImagenes(){
+    
+        document.pin1.src=imagenes[indiceImagenes];
+    
+    if (indiceImagenes < 1) {
+        indiceImagenes++; 
+        
+    }else{
+    indiceImagenes = 0;
+    }
+    
+    }
+    
+    setInterval(cambiarImagenes,2000);
+    
+    });
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
